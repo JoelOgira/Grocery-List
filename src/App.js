5fa7a9fc86b99013ee2946bb96ad663b83ed112a
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-07-13 13:13:40 +0300

    Added Header and Content Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import Navbar from './Components/Navbar';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Content from './Components/Content';
+
 
 const App = () => {
+
+  const handleNameChanges = () => {
+    const names = ['Jane', 'Hoe', 'Kalulu'];
+    const randomNumber = Math.floor(Math.random() * names.length );
+    return names[randomNumber];
+  }
+
   return (
     <div className="App">
-      <Navbar />
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Header.js
@@ -0,0 +1,27 @@
+const Header = () => {
+  return (
+    <div className="navbar navbar-expand-sm bg-transparent py-4">
+        <div className="container">
+            <div className="navbar-brand">
+            <a href="/" className='py-2'>Groceries List</a>
+            </div>
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                    <a href="/" className="nav-link">Home</a>
+                </li>
+                <li className="nav-item">
+                    <a href="/services" className="nav-link">Services</a>
+                </li>
+                <li className="nav-item">
+                    <a href="/about" className="nav-link">About</a>
+                </li>
+                <li className="nav-item">
+                    <a href="/contact" className="nav-link">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Header
