65b59183c25c24f8764e04cda200a0750f98a0f0
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-07-14 05:04:02 +0300

    Added react icons

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaTrashAlt } from 'react-icons/fa';
 
 const Content = () => {
 
@@ -23,7 +24,16 @@ const Content = () => {
     <main>
         <div className="container">
             <h2 className="text-center display-4 py-3">Groceries List</h2>
-  
+            <ul class="list-group">
+              {items.map(item => (
+                <li className="list-group-item" key={item.id}>
+                <input className="form-check-input me-1 " type="checkbox" checked={item.checked} aria-label="..."/>
+                  <span className="px-3">{item.item}</span>
+                  {/* <i className="fas fa-trash-can pt-2" style={{ color: 'green', float: 'right', cursor: 'pointer' }}></i> */}
+                  <FaTrashAlt style={{ color: 'green', float: 'right', cursor: 'pointer' }} role="button" tabIndex="0" />
+                </li>
+              ))}
+            </ul>
         </div>
     </main>
     
