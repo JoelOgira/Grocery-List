eaba4912e62480a95b63c5fcd4568846b6b39ede
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-07-15 16:18:39 +0300

    Added Handle Check functionality

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -20,15 +20,22 @@ const Content = () => {
     }
   ]);
 
+  const handleCheck = (id) => {
+    const listItems = items.map((item) => 
+      item.id === id ? {...item, checked: !item.checked} : item
+      );
+      setItems(listItems);
+  }
+
   return (
     <main>
         <div className="container">
             <h2 className="text-center display-4 py-3">Groceries List</h2>
-            <ul class="list-group">
+            <ul className="list-group">
               {items.map(item => (
-                <li className="list-group-item" key={item.id}>
-                <input className="form-check-input me-1 " type="checkbox" checked={item.checked} aria-label="..."/>
-                  <span className="px-3">{item.item}</span>
+                <li className="list-group-item item" key={item.id}>
+                <input className="form-check-input" onChange={() => handleCheck(item.id)} type="checkbox" checked={item.checked} />
+                  <label className="px-3">{item.item}</label>
                   {/* <i className="fas fa-trash-can pt-2" style={{ color: 'green', float: 'right', cursor: 'pointer' }}></i> */}
                   <FaTrashAlt style={{ color: 'green', float: 'right', cursor: 'pointer' }} role="button" tabIndex="0" />
                 </li>
