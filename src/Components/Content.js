4668b015b4a7b9ff8f2c1d45712bbfd6f6d3c4f9
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-07-16 12:29:09 +0300

    Added Handle Delete Function

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -20,27 +20,38 @@ const Content = () => {
     }
   ]);
 
-  const handleCheck = (id) => {
-    const listItems = items.map((item) => 
+  const handleCheck = id => {
+    const listItems = items.map(item => 
       item.id === id ? {...item, checked: !item.checked} : item
       );
       setItems(listItems);
+      localStorage.setItem('shoppingList', JSON.stringify(listItems));  
+  }
+
+  const handleDelete = id => {
+    const listItems = items.filter(item => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem('shoppingList', JSON.stringify(listItems));
   }
 
   return (
     <main>
         <div className="container">
             <h2 className="text-center display-4 py-3">Groceries List</h2>
-            <ul className="list-group">
+            {items.length ? (
+              <ul className="list-group">
               {items.map(item => (
                 <li className="list-group-item item" key={item.id}>
                 <input className="form-check-input" onChange={() => handleCheck(item.id)} type="checkbox" checked={item.checked} />
-                  <label className="px-3">{item.item}</label>
+                  <label className="px-3" onDoubleClick={() => handleCheck(item.id)}>{item.item}</label>
                   {/* <i className="fas fa-trash-can pt-2" style={{ color: 'green', float: 'right', cursor: 'pointer' }}></i> */}
-                  <FaTrashAlt style={{ color: 'green', float: 'right', cursor: 'pointer' }} role="button" tabIndex="0" />
+                  <FaTrashAlt onClick={() => handleDelete(item.id)} style={{ color: 'green', float: 'right', cursor: 'pointer' }} role="button" tabIndex="0" />
                 </li>
               ))}
-            </ul>
+              </ul>
+            ) : (
+              <p className="text-center text-danger" style={{marginTop: '200px'}} >Your Grocery List is Empty !!</p>
+            )}
         </div>
     </main>
     
