d15ff9d2af8f4f802d0bf934318d6d01baf44b88
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-09-26 19:07:39 +0300

    Added useFetch hook to the Home grocery list

--- src/Components/Grocery List/Home.jsx
+++ src/Components/Grocery List/Home.jsx
@@ -3,23 +3,38 @@ import Title from './Title';
 import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 const Home = () => {
 
-    const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoppingList')));
-
+    const [items, setItems] = useState([]);
     const [newItem, setNewItem] = useState('');
-
     const [search, setSearch] = useState('');
+    const [fetchError, setfetchError] = useState(null);
+
+    const API_URL = 'http://localhost:8000/items';
+
+    useEffect(() => {
+        const fetchItems = async () => {
+            try {
+                const response = await fetch(API_URL);
+                if (!response.ok) throw Error('Could not fetch requested data');
+                const listItems = await response.json();
+                setItems(listItems);
+                setfetchError(null);
+            } catch (error) {
+                setfetchError(error.message);
+            }
+        }
+        (async () => await fetchItems())();
+    }, []);
 
     const addItem = item => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
     setItems(listItems);
-    localStorage.setItem('shoppingList', JSON.stringify(listItems)); 
     }
 
     const handleCheck = id => {
@@ -27,13 +42,11 @@ const Home = () => {
         item.id === id ? {...item, checked: !item.checked} : item
         );
         setItems(listItems);
-        localStorage.setItem('shoppingList', JSON.stringify(listItems));  
     }
 
     const handleDelete = id => {
     const listItems = items.filter(item => item.id !== id);
     setItems(listItems);
-    localStorage.setItem('shoppingList', JSON.stringify(listItems));
     }
 
     const handleSubmit = e => {
@@ -60,15 +73,19 @@ const Home = () => {
             setSearch={setSearch}
         />
 
-        <Content 
-            items={items.filter(item => ((item.item).toLowerCase().includes(search.toLowerCase())))}
-            handleCheck={handleCheck}
-            handleDelete={handleDelete}
-        />
+        <main>
+            {fetchError && <p className='text-center' style={{color: 'red'}}>{`Error: ${fetchError}`}</p>}
+            {!fetchError && <Content 
+                items={items.filter(item => ((item.item).toLowerCase().includes(search.toLowerCase())))}
+                handleCheck={handleCheck}
+                handleDelete={handleDelete}
+            />}
+            {!fetchError && <Footer 
+                length={items.length}
+            />}
+        </main>
         
-        <Footer 
-            length={items.length}
-        />
+        
     </div>
   )
 }
