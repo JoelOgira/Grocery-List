4b736649153e88f1f0e12c9a6e7bdc609448886f
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-09-30 14:58:49 +0300

    Configured the checked status of the items

--- src/Components/Grocery 2.0/Grocery.jsx
+++ src/Components/Grocery 2.0/Grocery.jsx
@@ -38,28 +38,40 @@ const Grocery = () => {
     
   }, [])
 
-  const addItem = async (item) => {
+  const addItem = async item => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item }
     const listItems = [...items, myNewItem]
     setItems(listItems);
 
-    const addOptions = {
+    const postOptions = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(myNewItem)
-    }
+    }  
 
-    const result = await apiRequest(API_URL, addOptions);
-    if (result) {setfetchError(result)}
+    const result = await apiRequest(API_URL, postOptions);
+    if (result) {setfetchError(result)};
 
   }
 
-  const handleCheck = id => {
+  const handleCheck = async id => {
     const listItems = items.map(item => item.id === id ? {...item, checked: !item.checked} : item );
     setItems(listItems);
+
+    const myItem = listItems.filter(item => item.id === id);
+    const updateOptions = {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({checked: myItem[0].checked})
+    };
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) {setfetchError(result)};
   }
 
   const handleDelete = id => {
