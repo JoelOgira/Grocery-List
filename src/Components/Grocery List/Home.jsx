86a590e18c43417eb276b2b6130024fbf566f7d0
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-09-30 17:07:37 +0300

    Added CRUD options to the original grocery list

--- src/Components/Grocery List/Home.jsx
+++ src/Components/Grocery List/Home.jsx
@@ -3,6 +3,7 @@ import Title from './Title';
 import AddItem from './AddItem';
 import Content from './Content';
 import Footer from './Footer';
+import apiRequest from '../Grocery 2.0/apiRequest';
 import { useState, useEffect } from 'react';
 
 
@@ -11,6 +12,7 @@ const Home = () => {
     const [items, setItems] = useState([]);
     const [newItem, setNewItem] = useState('');
     const [search, setSearch] = useState('');
+    const [isLoading, setisLoading] = useState(true);
     const [fetchError, setfetchError] = useState(null); 
 
     const API_URL = 'http://localhost:8000/items';
@@ -25,28 +27,68 @@ const Home = () => {
                 setfetchError(null);
             } catch (error) {
                 setfetchError(error.message);
+            } finally {
+                setisLoading(false);
             }
         }
-        (async () => await fetchItems())();
+        
+        setTimeout(() => {
+            (async () => await fetchItems())();
+        }, 3000);
+
+
     }, []);
 
-    const addItem = item => {
+    const addItem = async item => {
     const id = items.length ? items[items.length - 1].id + 1 : 1;
     const myNewItem = { id, checked: false, item };
     const listItems = [...items, myNewItem];
     setItems(listItems);
+
+    const postOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(myNewItem)
     }
 
-    const handleCheck = id => {
+    const result = await apiRequest(API_URL, postOptions);
+    if (result) {setfetchError(result)};
+
+    }
+
+    const handleCheck = async id => {
     const listItems = items.map(item => 
         item.id === id ? {...item, checked: !item.checked} : item
         );
         setItems(listItems);
+    
+    const myItem = listItems.filter(item => item.id === id);
+    
+    const updateOptions = {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ checked: myItem[0].checked })
+    };
+
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, updateOptions);
+    if (result) {setfetchError(result)};
+
     }
 
-    const handleDelete = id => {
+    const handleDelete = async id => {
     const listItems = items.filter(item => item.id !== id);
     setItems(listItems);
+
+    const deleteOptions = {method: 'DELETE'};
+    const reqUrl = `${API_URL}/${id}`;
+    const result = await apiRequest(reqUrl, deleteOptions);
+    if (result) {setfetchError(result)};
+
     }
 
     const handleSubmit = e => {
@@ -74,13 +116,14 @@ const Home = () => {
         />
 
         <main>
-            {fetchError && <p className='text-center' style={{color: 'red'}}>{`Error: ${fetchError}`}</p>}
-            {!fetchError && <Content 
+            {isLoading && <p className='text-center my-4 py-4' style={{color: 'green'}}>Fetching Data ...</p>}
+            {fetchError && <p className='text-center my-4 py-4' style={{color: 'red'}}>{`Error: ${fetchError}`}</p>}
+            {!fetchError && !isLoading && <Content 
                 items={items.filter(item => ((item.item).toLowerCase().includes(search.toLowerCase())))}
                 handleCheck={handleCheck}
                 handleDelete={handleDelete}
             />}
-            {!fetchError && <Footer 
+            {!fetchError && !isLoading && <Footer 
                 length={items.length}
             />}
         </main>
