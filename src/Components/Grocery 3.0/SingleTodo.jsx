357ac0b55286dc600346e239f9f21683603190fe
JoelOgira-2020 <78035672+JoelOgira-2020@users.noreply.github.com>
2022-10-15 18:21:11 +0300

    Updated styling in the ToDo List application

--- src/Components/Grocery 3.0/SingleTodo.jsx
+++ src/Components/Grocery 3.0/SingleTodo.jsx
@@ -6,7 +6,7 @@ const SingleTodo = ({ todos, handleDelete }) => {
             <li className="list-group-item my-2" key={todo.id}>
                 <input type="checkbox" className="form-check-input mx-2" style={{border: '1px solid black'}}/>
                 <label className="form-check-label">{todo.todo}</label>
-                <FaTrashAlt role="button" className="mt-1" style={{float: 'right' }} onClick={() => handleDelete(todo.id)}/>
+                <FaTrashAlt role="button" className="mt-1 delete-todo" style={{float: 'right' }} onClick={() => handleDelete(todo.id)}/>
             </li>
         )}
     </main>
